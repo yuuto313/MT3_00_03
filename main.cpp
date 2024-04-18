beb5e97dd450de0a2e4c06b1d68cb741751ac9de
ozawa yuuto <146906726+yuuto313@users.noreply.github.com>
2024-04-18 19:34:06 +0900

    MT3_00_03課題

--- main.cpp
+++ main.cpp
@@ -25,6 +25,7 @@ Matrix4x4 MakeScaleMatrix(const Vector3& scale){
 	result.m[3][3] = 1;
 	return result;
 }
+
 //座標変換
 Vector3 Transform(const Vector3&vector,const Matrix4x4& matrix){
 	Vector3 result{};
